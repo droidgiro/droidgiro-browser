ba64b0a01d2d3aa532b15f53fcb934630414ecdd
Johan Nilsson <github@markupartist.com>
2011-01-11 21:58:43 +0100

    Changed endpoint for the api to invoices also added a api to list the recent added invoices.

--- app/agiro.py
+++ app/agiro.py
@@ -5,7 +5,11 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
 import os
+from datetime import datetime, time, date
+import time
 from django.utils import simplejson
+
+SIMPLE_TYPES = (int, long, float, bool, dict, basestring, list)
 
 class Invoice(db.Model):
     user = db.UserProperty()
@@ -13,6 +17,26 @@ class Invoice(db.Model):
     amount = db.StringProperty()
     document_type = db.StringProperty()
     date = db.DateTimeProperty(auto_now_add=True)
+
+    def to_dict(self):
+        model = self
+        output = {}
+        for key, prop in model.properties().iteritems():
+            value = getattr(model, key)
+
+            if value is None or isinstance(value, SIMPLE_TYPES):
+                output[key] = value
+            elif isinstance(value, date):
+                # Convert date/datetime to ms-since-epoch ("new Date()").
+                ms = time.mktime(value.utctimetuple()) * 1000
+                ms += getattr(value, 'microseconds', 0) / 1000
+                output[key] = int(ms)
+            elif isinstance(value, db.Model):
+                output[key] = to_dict(value)          
+            #else:
+                #raise ValueError('cannot encode ' + repr(prop))
+        output["id"] = model.key().id()
+        return output
 
 class MainPage(webapp.RequestHandler):
     def get(self):
@@ -36,11 +60,39 @@ class MainPage(webapp.RequestHandler):
         self.response.out.write(template.render(path, template_values))
 
 class InvoiceHandler(webapp.RequestHandler):
-    def post(self):
+
+    def get(self, invoice_id):
+        #if not self.request.headers.get('"X-Same-Domain"'):
+        #    self.response.set_status(400)
+        #    self.response.out.write('Missing header')
+        #    return
+
+        user = users.get_current_user()
+        if not user:
+            self.response.set_status(401)
+            self.response.headers.add_header("Content-Type", 'application/json; charset=utf-8')
+            self.response.out.write(simplejson.dumps("Unauthorized"))
+            return
+
+        if invoice_id is None:
+            invoice_query = db.GqlQuery("SELECT * FROM Invoice WHERE user = :1 ORDER BY date DESC LIMIT 10", user)
+            invoice_list = invoice_query.fetch(10)
+
+            response_dict = [i.to_dict() for i in invoice_list]
+            json_response = simplejson.dumps(response_dict)
+        else:
+            json_response = simplejson.dumps("Not supported...")
+
+        self.response.headers.add_header("Content-Type", 'application/json; charset=utf-8')
+        self.response.out.write(json_response)
+
+    def post(self, invoice_id):
         user = users.get_current_user()
 
         if not user:
-            self.redirect(users.create_login_url(self.request.uri))
+            self.response.set_status(401)
+            self.response.headers.add_header("Content-Type", 'application/json; charset=utf-8')
+            self.response.out.write(simplejson.dumps("Unauthorized"))
             return
 
         invoice = Invoice()
@@ -68,7 +120,7 @@ class InvoiceHandler(webapp.RequestHandler):
 
 application = webapp.WSGIApplication(
                                      [('/', MainPage),
-                                      ('/add', InvoiceHandler)],
+                                      ('/invoices(?:/(\d+))?', InvoiceHandler)],
                                      debug=True)
 
 def main():
