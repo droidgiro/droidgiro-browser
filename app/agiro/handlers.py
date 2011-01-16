fcb3e16a22923fb0f3d45de26b1fb99945f123aa
Johan Nilsson <github@markupartist.com>
2011-01-16 20:53:04 +0100

    Added TODOs.

--- app/agiro/handlers.py
+++ app/agiro/handlers.py
@@ -9,6 +9,7 @@ import os
 from datetime import datetime, time, date
 import time
 import random
+import md5
 from django.utils import simplejson
 from agiro.models import Invoice
 
@@ -42,19 +43,9 @@ class InvoiceHandler(webapp.RequestHandler):
         self.response.set_status(201)
         self.response.out.write(response)
 
-    def get(self, invoice_id):
-        token = channel.create_channel(str(1000))
-
-        template_values = {
-            'token': token,
-        }
-
-        path = os.path.join(os.path.dirname(__file__), '../templates/invoices.html')
-        self.response.out.write(template.render(path, template_values))
-
 class RegisterHandler(webapp.RequestHandler):
     def get(self):
-        #identifier = 1000 #random.randint(1000, 9999)
+        # TODO: Generate pin and channel name and verify that it's available
         identifier = random.randint(1000, 9999)
         token = channel.create_channel(str(identifier))
 
@@ -63,4 +54,17 @@ class RegisterHandler(webapp.RequestHandler):
             'token': token,
             'identifier': identifier,
         }))
+
+    def post(self):
+        # TODO: Verify pin and channel.
+        pin = self.request.get('pin')
+        m = md5.new()
+        m.update(str(random.randint(1000, 99999)))
+        channel = m.hexdigest()
+
+        self.response.headers.add_header("Content-Type", 'application/json; charset=utf-8')
+        self.response.out.write(simplejson.dumps({
+            'channel': channel,
+        }))
+
 
